9ea4ccbcf0637309e0925550955ef4b59d42e565
Dinesh kumar <87605928+DineshK3012@users.noreply.github.com>
2022-07-26 00:04:15 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// function onHover(e) {
+//     let que = document.querySelectorAll('section .ques que');
+//     console.log(que);
+
+//     console.log("clicked");
+
+//     que.forEach(element => {
+
+//     });
+
+//     e.classList.add('onHover');
+// }       
